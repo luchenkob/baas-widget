bb42997ee32b41d9d8cb70f1f7fc0d38d5e86c49
Bogdan Luchenko <luchenkob@gmail.com>
2020-04-16 14:24:48 +0300

    Tickets: Allow height of widget to be specified, Fix style names to avoid clashes with other Bootstrap websites

--- com.elsten.bliss.baas.widget/src/containers/App.jsx
+++ com.elsten.bliss.baas.widget/src/containers/App.jsx
@@ -1,36 +1,42 @@
-import React, {useReducer } from "react";
+import React, { useReducer } from "react";
 import ViewsManager from "../views/ViewsManager";
 import Preloader from "../components/Preloader/Preloader"
 import Notification from "../components/Notification/Notification"
 import { Context } from "../context/context";
 import reducer from "../context/reducer";
+import {appSettings} from "../defines/settings";
 
-const App = () => {
+const App = ({config}) => {
 
-  const [state, dispatch] = useReducer(reducer, { 
+  const [state, dispatch] = useReducer(reducer, {
     isProcessing: false,
     isNotification: false,
     isBussy: false,
     len: 0,
     cur: 0,
     files: {},
-    origFiles:[],
+    origFiles: [],
     assessments: [],
     errors: [],
-    step:1,
+    step: 1,
     activeAlbum: '',
     activeAssessment: 0,
     processingMessage: 'Loading',
     notificationMessage: "",
-    notificationType: "primary", }
-    );
+    notificationType: "primary",
+  }
+  );
+
+  console.log(config)
 
   return (
-    <Context.Provider value={{dispatch}}>
-      <Notification {...state}/>
-      <Preloader {...state}/>
-      <ViewsManager {...state}/>
-    </Context.Provider>
+    <div className="baas" style={{height: config.height ? config.height : appSettings.defaultConfig.height}}>
+      <Context.Provider value={{ dispatch }}>
+        <Notification {...state} />
+        <Preloader {...state} />
+        <ViewsManager {...state} />
+      </Context.Provider>
+    </div>
   );
 }
 
--- com.elsten.bliss.baas.widget/src/defines/settings.js
+++ com.elsten.bliss.baas.widget/src/defines/settings.js
@@ -1,3 +1,7 @@
 export const appSettings = {
-  api: "https://virtserver.swaggerhub.com/zorasic/baa-s/0.0.0/"
+  api: "https://virtserver.swaggerhub.com/zorasic/baa-s/0.0.0/",
+
+  defaultConfig: {
+    height:"400px",
+  }
 }
