a0a73b43f5715f35d55090e25c5141eece94b562
Bogdan Luchenko <luchenkob@gmail.com>
2020-04-24 19:32:18 +0300

    Ticket: #46 GET to assessment uses the incorrect ID

--- com.elsten.bliss.baas.widget/src/views/ViewResult/ViewResult.jsx
+++ com.elsten.bliss.baas.widget/src/views/ViewResult/ViewResult.jsx
@@ -69,7 +69,7 @@ const ViewResult = (state) => {
     if(config.rules) data.rules = config.rules
 
     ApiService.post("assessment", data, config).then(result => {
-      localStorage.setItem('assessment', '542');
+      localStorage.setItem('assessment', result.data);
       dispatch({ type: "SET_STEP", data: { step: 3 } })
     }, error => {
       dispatch({ type: "SET_NOTIFICATION", data: {isNotification: true, notificationMessage: `${error}`, notificationType: "danger", isBussy: false} })
