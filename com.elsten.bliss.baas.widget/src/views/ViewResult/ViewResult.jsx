12068ca907f2e5a9532bc2e8232f2b79015d7b10
Bogdan Luchenko <luchenkob@gmail.com>
2020-05-02 12:01:27 +0300

    Ticket: #62 Don't send empty COVER_ART members

--- com.elsten.bliss.baas.widget/src/views/ViewResult/ViewResult.jsx
+++ com.elsten.bliss.baas.widget/src/views/ViewResult/ViewResult.jsx
@@ -14,7 +14,7 @@ const ViewResult = (state) => {
   const { dispatch, config } = useContext(Context);
 
   const handleUploadAgain = () => {
-    dispatch({ type: "SET_STEP", data: { step: 1, isNotification: false, isProcessing: false, isBussy: false}})
+    dispatch({ type: "SET_STEP", data: { step: 1, isNotification: false, isProcessing: false, isBussy: false } })
   }
 
   const handleAssess = () => {
@@ -22,42 +22,47 @@ const ViewResult = (state) => {
     const { origFiles } = state;
     let data = { "storageNodes": [] }
 
-    dispatch({ 
+    dispatch({
       type: "SET_NOTIFICATION", data: {
-      isProcessing: true,
-      processingMessage: "Data is being processed",
-      isBussy: true} 
+        isProcessing: true,
+        processingMessage: "Data is being processed",
+        isBussy: true
+      }
     })
 
     origFiles.forEach((file) => {
 
-      if(file.type != "image/jpeg" && file.type != "image/png") {
-        data["storageNodes"].push(
-          {
-            "lib": "/",
-            "path": file.path,
-            "mimeType": file.type,
-            "duration": file.format.duration ? file.format.duration * 1000 : null,
-            "fields": {
-              "ALBUM_NAME": file.common.album ? file.common.album : null,
-              "ARTIST": file.common.artist ? file.common.artist : null,
-              "YEAR": file.common.year ? file.common.year : null,
-              "GENRE": file.common.genre ? file.common.genre : null,
-              "TRACK_NAME": file.common.title ? file.common.title : null,
-              "ALBUM_ARTIST": file.common.albumartist ? file.common.albumartist : null,
-              "COVER_ART": file.common.picture ? {
-                "width": sizeOf(file.common.picture[0].data).width,
-                "height": sizeOf(file.common.picture[0].data).height,
-                "sizeBytes": file.common.picture[0].data.byteLength,
-                "codec": getFilteredCodec(file.common.picture[0].format),
-              } : {},
-              "COMPILATION": true,
-              "TRACK_NUMBER": file.common.track.no ? file.common.track.no : null,
-              "DISC_NUMBER": file.common.disk.no ? file.common.disk.no : null,
-            }
+      if (file.type != "image/jpeg" && file.type != "image/png") {
+
+        let json =
+        {
+          "lib": "/",
+          "path": file.path,
+          "mimeType": file.type,
+          "duration": file.format.duration ? file.format.duration * 1000 : null,
+          "fields": {
+            "ALBUM_NAME": file.common.album ? file.common.album : null,
+            "ARTIST": file.common.artist ? file.common.artist : null,
+            "YEAR": file.common.year ? file.common.year : null,
+            "GENRE": file.common.genre ? file.common.genre : null,
+            "TRACK_NAME": file.common.title ? file.common.title : null,
+            "ALBUM_ARTIST": file.common.albumartist ? file.common.albumartist : null,
+            "COMPILATION": true,
+            "TRACK_NUMBER": file.common.track.no ? file.common.track.no : null,
+            "DISC_NUMBER": file.common.disk.no ? file.common.disk.no : null,
           }
-        );
-      }else {
+        }
+
+        if (file.common.picture) json.fields.COVER_ART = {
+          "width": sizeOf(file.common.picture[0].data).width,
+          "height": sizeOf(file.common.picture[0].data).height,
+          "sizeBytes": file.common.picture[0].data.byteLength,
+          "codec": getFilteredCodec(file.common.picture[0].format)
+        }
+
+        data["storageNodes"].push(json);
+
+      } else {
         data["storageNodes"].push(
           {
             "lib": "/",
@@ -71,18 +76,20 @@ const ViewResult = (state) => {
       }
     });
 
-    if(config.rules) data.rules = config.rules
+    console.log(data)
+
+    if (config.rules) data.rules = config.rules
 
     if (config.endpoint !== "local") {
       ApiService.post("assessment", data, config).then(result => {
-        if(result.headers.location) {
+        if (result.headers.location) {
           localStorage.setItem('assessment', result.headers.location);
           dispatch({ type: "SET_STEP", data: { step: 3 } })
         }
       }, error => {
-        dispatch({ type: "SET_NOTIFICATION", data: {isNotification: true, notificationMessage: `${error}`, notificationType: "danger", isProcessing: false, isBussy: false} })
+        dispatch({ type: "SET_NOTIFICATION", data: { isNotification: true, notificationMessage: `${error}`, notificationType: "danger", isProcessing: false, isBussy: false } })
       })
-    }else{
+    } else {
       dispatch({ type: "SET_STEP", data: { step: 3 } })
     }
 
