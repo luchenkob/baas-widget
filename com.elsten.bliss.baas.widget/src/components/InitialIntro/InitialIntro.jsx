6a15c30479553c81ed369b13a29ddde81dffec71
Bogdan Luchenko <luchenkob@gmail.com>
2020-05-08 16:42:53 +0300

    Ticket: #89 Parameterise widget header and title

--- com.elsten.bliss.baas.widget/src/components/InitialIntro/InitialIntro.jsx
+++ com.elsten.bliss.baas.widget/src/components/InitialIntro/InitialIntro.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, {useContext} from "react";
 import { useTranslation } from "react-i18next";
+import { Context } from "../../context/context";
 
 const InitialIntro = () => {
 
   const { t } = useTranslation();
+  const { config } = useContext(Context);
 
   return (
     <div>
-      <h2 className="text-center text-primary">{t('BaaS Widget')}</h2>
-      <p className="mw-800 mx-auto text-center">{t('Description of the widget')}</p>
+      <h2 className="text-center text-primary">{t(config.title)}</h2>
+      <p className="mw-800 mx-auto text-center" dangerouslySetInnerHTML={{ __html: t(config.introHtml) }}></p>
     </div>
   );
 }
--- com.elsten.bliss.baas.widget/src/views/ViewUploader/ViewUploader.jsx
+++ com.elsten.bliss.baas.widget/src/views/ViewUploader/ViewUploader.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, {useContext} from "react";
 import Uploader from "../../components/Uploader/Uploader";
 import InitialIntro from "../../components/InitialIntro/InitialIntro";
 import LayoutIntro from "../../layouts/LayoutIntro/LayoutIntro"
+import { Context } from "../../context/context";
 
 const ViewUploader = (state) => {
 
+  const { config } = useContext(Context);
+
   return (
-    <LayoutIntro slots={[
-      <InitialIntro />,
-      <Uploader {...state} />
-    ]}/>
+    !config.title && !config.introHtml ?
+      <LayoutIntro slots={[<></>,
+        <Uploader {...state} />
+      ]} /> :
+      <LayoutIntro slots={[
+        <InitialIntro />,
+        <Uploader {...state} />
+      ]} />
   );
 }
 
