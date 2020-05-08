0268596bb129584fa8d8e7463ef1240ab14b98ad
Bogdan Luchenko <luchenkob@gmail.com>
2020-05-08 20:25:51 +0300

    Beack to 100%

--- com.elsten.bliss.baas.widget/src/defines/config.js
+++ com.elsten.bliss.baas.widget/src/defines/config.js
@@ -1,7 +1,7 @@
 export const appDefaultConfig = {
   endpoint: "https://vnlqoz47el.execute-api.us-east-1.amazonaws.com/testing/",
   localapi: "/localapi/",
-  height:"400px",
+  height:"100%",
   queryDelay: 5000,
   completeHtml: "To find the missing artwork for your albums,<br/> <a href='/download.html'>download and install bliss</a>",
   completeLabel: "Download and install bliss",
