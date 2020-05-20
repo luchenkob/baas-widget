bf4e87490cd88effafe0057ca20e62ea5c396840
Bogdan Luchenko <luchenkob@gmail.com>
2020-05-20 15:44:06 +0300

    #101 Spinner has disappeared

--- com.elsten.bliss.baas.widget/src/components/Icon/index.jsx
+++ com.elsten.bliss.baas.widget/src/components/Icon/index.jsx
@@ -10,7 +10,7 @@ const Icon = ({ variant, className }) => {
       case "times":
         return (<svg className={`${_p}app-icon ${className}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M19.4,16l11-11c0.3-0.3,0.3-0.8,0-1l-2.4-2.4c-0.3-0.3-0.8-0.3-1,0l-11,11L5,1.6c-0.3-0.3-0.8-0.3-1,0L1.6,3.9c-0.3,0.3-0.3,0.8,0,1l11,11l-11,11c-0.3,0.3-0.3,0.8,0,1l2.4,2.4c0.3,0.3,0.8,0.3,1,0l11-11l11,11c0.3,0.3,0.8,0.3,1,0l2.4-2.4c0.3-0.3,0.3-0.8,0-1L19.4,16z" /></svg>);
       case "loader":
-        return (<svg className={`${_p}circular`} viewBox="25 25 50 50"><circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth="4" strokeMiterlimit="10" /></svg>)
+        return (<svg className={`${_p}circular`} viewBox="25 25 50 50"><circle className={`${_p}path`} cx="50" cy="50" r="20" fill="none" strokeWidth="4" strokeMiterlimit="10" /></svg>)
       case "done":
         return (<svg className={`${_p}app-icon ${className}`} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="35.2px" height="26.8px" viewBox="0 0 35.2 26.8"><path d="M11.2,21.2l-8.3-8.3L0,15.7l11.2,11.2l24-24L32.3,0L11.2,21.2z"/></svg>)
       case "info":
