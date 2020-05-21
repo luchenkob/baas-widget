d2cbd7676d7c34f8cd003f7453ee958f61e83c30
Bogdan Luchenko <luchenkob@gmail.com>
2020-05-21 11:28:01 +0300

    #104 Add a help button to each step (except the first)

--- com.elsten.bliss.baas.widget/src/defines/config.js
+++ com.elsten.bliss.baas.widget/src/defines/config.js
@@ -5,6 +5,10 @@ export const appDefaultConfig = {
   queryDelay: 5000,
   prefix: "baasZcx-",
   showPoweredBy: true,
+  previewStepHelpTitleHtml: "Scanned files",
+  previewStepHelpContentHtml: "Displays the scanned files.",
+  assessmentStepHelpTitleHtml: "Assessment files",
+  assessmentStepHelpContentHtml: "Displays the assessment files.",
   completeHtml: "To find the missing artwork for your albums,<br/> <a href='/download.html'>download and install bliss</a>",
   completeLabel: "Download and install bliss",
   completeLink: "./download.html"
